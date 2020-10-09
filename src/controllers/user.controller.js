6c5ddc67c5051e459cec44bb645fc130485f6c92
JoselitoNascimento <joselito_nascimento@yahoo.com.br>
2020-10-09 01:04:29 -0300

    inserido rota incluir usuario e validações

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,44 @@
+const db = require("../config/database");
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
+const { validationResult } = require('express-validator');
+
+// ==> Método responsável por criar um novo 'Usuario':
+
+exports.createUser = async (req, res) => {
+  const { errors } = validationResult(req);
+  if (errors.length > 0) {
+    return res.status(400).send({ message: errors })
+  }
+  const { user_name, email, perfil_id, ativo, dt_inc, us_inc, entidade_id } = req.body;
+  const password = bcrypt.hashSync(req.body.password, salt)
+  const { rows } = await db.query(
+    "INSERT INTO usuarios (user_name, email, password, perfil_id, ativo, dt_inc, us_inc, entidade_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+    [user_name, email, password, perfil_id, ativo, dt_inc, us_inc, entidade_id]
+  );
+
+  res.status(201).send({
+    message: "user added successfully!",
+    body: {
+      product: { user_name, email, password, perfil_id, ativo, dt_inc, us_inc, entidade_id }
+    },
+  });
+};
+
+// ==> Método responsável por listar todos os 'Usuarios':
+exports.listAllUsers = async (_, res) => {
+  const response = await db.query('SELECT user_name, email, password, perfil_id, ativo, entidade_id FROM usuarios ORDER BY user_name ASC');
+  res.status(200).send(response.rows);
+};
+
+// ==> Método responsável por logar o 'Usuario':
+exports.loginUser = async (req, res) => {
+  const email = req.body.email;
+  const password = bcrypt.hashSync(req.body.password, salt);
+  console.log(email + ' - ' + password);
+  const response = await db.query('SELECT user_name, perfil_id, entidade_id FROM usuarios WHERE email = $1 AND password = $2 ',
+    [email, password]
+  );
+  res.status(200).send(response.rows);
+};
+
---
+++ src/routes/user.routes.js
@@ -0,0 +1,30 @@
+// @ts-nocheck
+/**
+ * Arquivo: src/routes/user.routes.js
+ * Descrição: arquivo responsável pelas rotas da api relacionado a classe 'User'.
+ * Data: 04/03/2020
+ * Author Glaucia Lemos
+ */
+
+const router = require('express-promise-router')();
+const { check } = require('express-validator');
+const userController = require('../controllers/user.controller');
+
+// ==> Definindo as rotas do CRUD - 'User':
+
+// ==> Rota responsável por listar todos os 'Usuarios': (GET): localhost:3000/users
+router.get('/users', userController.listAllUsers);
+
+// ==> Rota responsável por criar um novo 'usuarios': (POST): localhost:3000/users
+router.post('/users', [
+  check('email').isEmail().withMessage("email inválido!"),
+  check('password').isLength({ min: 8, max: 20 }).withMessage("tamanho de senha inválido!"),
+  check('perfil_id').isInt({ min: 1 }).withMessage("perfil de usuário inválido!"),
+  check('entidade_id').isInt({ min: 1 }).withMessage("código do usuário inválido no cadastro de entidades!"),
+  check('ativo').isIn(['A', 'I']).withMessage("situação do usuário inválida!")
+], userController.createUser);
+
+// ==> Rota responsável por logar 'Usuario': (GET): localhost:3000/user
+router.get('/user', userController.loginUser);
+
+module.exports = router;
