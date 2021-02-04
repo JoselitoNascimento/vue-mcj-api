c931e12b5b1ab0d72a791711a204fb14fcde8d6c
joselito Nascimento <joselito_nascimento@yahoo.com.br>
2021-02-04 18:57:34 -0300

    ajustado consulta cnae

--- src/controllers/cnae.controller.js
+++ src/controllers/cnae.controller.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.listCNAEs = async (req, res) => {
   const { cnae } = req.body;
   const http = axios.create({
-    baseURL: 'https://servicodados.ibge.gov.br/api/v2/cnae/classes/' + (cnae == undefined ? '' : cnae)
+    baseURL: 'https://servicodados.ibge.gov.br/api/v2/cnae/subclasses/' + (cnae == undefined ? '' : cnae)
   });
   const response = await http.get();
   res.status(200).send(response.data) ;
